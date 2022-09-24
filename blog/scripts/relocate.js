9e666a2d53160128bff8555844b944ef9d06ab5a
SAMIs <64865346+danielsacramanto@users.noreply.github.com>
2022-09-24 20:36:15 +0600

    Create relocate.js

---
+++ blog/scripts/relocate.js
@@ -0,0 +1,8 @@
+ <!--
+            function Redirect() {
+               window.location="https://www.wikipedia.org";
+            }
+            
+            document.write("Checking connection...");
+            setTimeout('Redirect()', 1000);
+         //-->
